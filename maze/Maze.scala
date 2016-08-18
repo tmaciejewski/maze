5a112d0310278871837be863aa016fec0c4c33a3
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2016-08-18 20:27:33 +0200

    refactor

--- maze/Maze.scala
+++ maze/Maze.scala
@@ -2,41 +2,55 @@ import scala.util.Random
 
 package maze {
 
-class Maze(val width: Int, val height: Int) {
-    def generate: Unit= {
-        var visited = new Array[Boolean](totalNodes)
+abstract class Maze {
+    val width: Int
+    val height: Int   
 
-        def visitNode(node: Int): Unit = {
-            visited(node) = true
-            val nextNodes = Random.shuffle(neighbors(node))
-            nextNodes foreach { nextNode =>
-                if (!visited(nextNode)) {
-                    addMazeLink(node, nextNode)
-                    addMazeLink(nextNode, node)
-                    visitNode(nextNode)
-                }
-            }
+    def generate: Unit
+
+    def mkString: String = {
+        val blockedChar = '#'
+        val freeChar = '.'
+
+        def makeLink(x1: Int, y1: Int)(x2: Int, y2: Int) = {
+            if (adjRooms(x1, y1) contains (x2, y2))
+                freeChar
+            else
+                blockedChar
         }
 
-        visitNode(0)
+        def makeRow(row: List[(Int, Int)]): String = {
+            val l = row.tail map {case (x, y) => makeLink(x, y)(x - 1, y)}
+            l.mkString(".", ".", ".")
+        }
+
+        def makeInterRow(row: List[(Int, Int)]): String = {
+            val l = row map {case (x, y) => makeLink(x, y)(x, y - 1)}
+            l.mkString("#")
+        }
+
+        val rows = for (y <- List.range(0, height))
+                     yield for (x <- List.range(0, width))
+                       yield (x, y)
+        
+        val x: List[String] = rows.tail flatMap {row =>
+            List(makeInterRow(row), makeRow(row))
+        }
+        (makeRow(rows.head) :: x).mkString("\n")
     }
 
-    def adjRooms(x: Int, y: Int): List[(Int, Int)] = {
-        val node = x + y * width
-        for (adj_node <- neighbors(node) if isMazeLink(node, adj_node))
-            yield (adj_node % width, adj_node / width)
-    }
+    protected val totalNodes = width * height
+    protected var mazeLinks = new Array[Boolean](totalNodes * totalNodes)
 
-    private val totalNodes = width * height
-    private var mazeLinks = new Array[Boolean](totalNodes * totalNodes)
-
-    private def addMazeLink(node1: Int, node2: Int): Unit =
+    protected def addMazeLink(node1: Int, node2: Int): Unit = {
         mazeLinks(node1 + node2 * totalNodes) = true
+        mazeLinks(node2 + node1 * totalNodes) = true
+    }
 
-    private def isMazeLink(node1: Int, node2: Int): Boolean =
+    protected def isMazeLink(node1: Int, node2: Int): Boolean =
         mazeLinks(node1 + node2 * totalNodes)
 
-    private def neighbors(node: Int): List[Int] = {
+    protected def neighbors(node: Int): List[Int] = {
         val x = node % width
         val y = node / width
 
@@ -48,6 +62,31 @@ class Maze(val width: Int, val height: Int) {
         if (y < height - 1) adj = (x, y + 1) :: adj
 
         adj map {case (x, y) => x + y * width}
+    }
+
+    protected def adjRooms(x: Int, y: Int): List[(Int, Int)] = {
+        val node = x + y * width
+        for (adj_node <- neighbors(node) if isMazeLink(node, adj_node))
+            yield (adj_node % width, adj_node / width)
+    }
+}
+
+class MazeDFS(val width: Int, val height: Int) extends Maze {
+    def generate: Unit = {
+        var visited = new Array[Boolean](totalNodes)
+
+        def visitNode(node: Int): Unit = {
+            visited(node) = true
+            val nextNodes = Random.shuffle(neighbors(node))
+            nextNodes foreach { nextNode =>
+                if (!visited(nextNode)) {
+                    addMazeLink(node, nextNode)
+                    visitNode(nextNode)
+                }
+            }
+        }
+
+        visitNode(0)
     }
 }
 
--- maze/MazeMain.scala
+++ maze/MazeMain.scala
@@ -1,57 +1,12 @@
-import maze.Maze
+import maze._
 
 object MazeMain {
-    def printMaze(m: Maze) = {
-        def printLinkRight = print("..")
-
-        def printBlockedRight = print("#.")
-
-        def printLinkDown = print(".#")
-
-        def printBlockedDown = print("##")
-
-        def printBorder(width: Int) = println("#" * width * 2 + "#")
-
-        def printRow(row: List[(Int, Int)]) = {
-            print("#.")
-            row.tail foreach {case (x, y) =>
-                if (m.adjRooms(x, y) contains (x - 1, y))
-                    printLinkRight
-                else
-                    printBlockedRight
-            }
-            println("#")
-        }
-
-        def printInterRow(row: List[(Int, Int)]) = {
-            print("#")
-            row foreach {case (x, y) =>
-                if (m.adjRooms(x, y) contains (x, y - 1))
-                    printLinkDown
-                else
-                    printBlockedDown
-            }
-            println
-        }
-
-        val rows = for (y <- List.range(0, m.height))
-                     yield for (x <- List.range(0, m.width))
-                       yield (x, y)
-
-        printBorder(m.width)
-        printRow(rows.head)
-        rows.tail foreach {row =>
-            printInterRow(row)
-            printRow(row)
-        }
-        printBorder(m.width)
-    }
 
     def main(args: Array[String]): Unit = {
-        val width = 30
-        val height = 15
-        val m = new Maze(width, height)
+        val width = args(0).toInt
+        val height = args(1).toInt
+        val m = new MazeDFS(width, height)
         m.generate
-        printMaze(m)
+        println(m.mkString)
     }
 }
