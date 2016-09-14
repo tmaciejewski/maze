b0574aee1791faa1909525e507afab17d59dc83b
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2016-09-14 17:35:01 +0200

    maze: replace mkString with isWall

--- maze/Maze.scala
+++ maze/Maze.scala
@@ -4,89 +4,85 @@ package maze {
 
 abstract class Maze {
     val width: Int
-    val height: Int   
+    val height: Int
 
     def generate: Unit
 
-    def mkString: String = {
-        val blockedChar = '#'
-        val freeChar = '.'
-
-        def makeLink(x1: Int, y1: Int)(x2: Int, y2: Int) = {
-            if (adjRooms(x1, y1) contains (x2, y2))
-                freeChar
-            else
-                blockedChar
-        }
-
-        def makeRow(row: List[(Int, Int)]): String = {
-            val l = row.tail map {case (x, y) => makeLink(x, y)(x - 1, y)}
-            l.mkString(".", ".", ".")
-        }
-
-        def makeInterRow(row: List[(Int, Int)]): String = {
-            val l = row map {case (x, y) => makeLink(x, y)(x, y - 1)}
-            l.mkString("#")
-        }
-
-        val rows = for (y <- List.range(0, height))
-                     yield for (x <- List.range(0, width))
-                       yield (x, y)
-        
-        val x: List[String] = rows.tail flatMap {row =>
-            List(makeInterRow(row), makeRow(row))
-        }
-        (makeRow(rows.head) :: x).mkString("\n")
-    }
-
-    protected val totalNodes = width * height
-    protected var mazeLinks = new Array[Boolean](totalNodes * totalNodes)
-
-    protected def addMazeLink(node1: Int, node2: Int): Unit = {
-        mazeLinks(node1 + node2 * totalNodes) = true
-        mazeLinks(node2 + node1 * totalNodes) = true
-    }
-
-    protected def isMazeLink(node1: Int, node2: Int): Boolean =
-        mazeLinks(node1 + node2 * totalNodes)
-
-    protected def neighbors(node: Int): List[Int] = {
-        val x = node % width
-        val y = node / width
-
-        var adj: List[(Int, Int)] = List()
-
-        if (x > 0) adj = (x - 1, y) :: adj
-        if (x < width - 1) adj = (x + 1, y) :: adj
-        if (y > 0) adj = (x, y - 1) :: adj
-        if (y < height - 1) adj = (x, y + 1) :: adj
-
-        adj map {case (x, y) => x + y * width}
-    }
-
-    protected def adjRooms(x: Int, y: Int): List[(Int, Int)] = {
-        val node = x + y * width
-        for (adj_node <- neighbors(node) if isMazeLink(node, adj_node))
-            yield (adj_node % width, adj_node / width)
-    }
+    def isWall(x: Int, y: Int): Boolean
 }
 
-class MazeDFS(val width: Int, val height: Int) extends Maze {
+class MazeDFS(maxWidth: Int, maxHeight: Int) extends Maze {
+    private val graphWidth = (maxWidth + 1) / 2
+    private val graphHeight = (maxHeight + 1) / 2
+
+    val width = 2 * graphWidth - 1
+    val height = 2 * graphHeight - 1
+
     def generate: Unit = {
-        var visited = new Array[Boolean](totalNodes)
+        var visited = new Array[Boolean](graphSize)
 
         def visitNode(node: Int): Unit = {
             visited(node) = true
             val nextNodes = Random.shuffle(neighbors(node))
             nextNodes foreach { nextNode =>
                 if (!visited(nextNode)) {
-                    addMazeLink(node, nextNode)
+                    addGraphLink(node, nextNode)
                     visitNode(nextNode)
                 }
             }
         }
 
         visitNode(0)
+    }
+
+    def isWall(x: Int, y: Int): Boolean = {
+        def isNode = (x % 2 == 0) && (y % 2 == 0)
+        def isLink = (x % 2 == 0) || (y % 2 == 0)
+
+        def isInterRow = y % 2 > 0
+
+        if (isNode)
+            false
+        else if (isLink) {
+            val nodeX = x / 2
+            val nodeY = y / 2
+            if (isInterRow)
+               !(adjRooms(nodeX, nodeY) contains (nodeX, nodeY + 1))
+            else
+               !(adjRooms(nodeX, nodeY) contains (nodeX + 1, nodeY))
+        } else
+            true
+    }
+
+    private val graphSize = graphWidth * graphHeight
+    private var graphLinks = new Array[Boolean](graphSize * graphSize)
+
+    private def addGraphLink(node1: Int, node2: Int): Unit = {
+        graphLinks(node1 + node2 * graphSize) = true
+        graphLinks(node2 + node1 * graphSize) = true
+    }
+
+    private def neighbors(node: Int): List[Int] = {
+        val x = node % graphWidth
+        val y = node / graphWidth
+
+        var adj: List[(Int, Int)] = List()
+
+        if (x > 0) adj = (x - 1, y) :: adj
+        if (x < graphWidth - 1) adj = (x + 1, y) :: adj
+        if (y > 0) adj = (x, y - 1) :: adj
+        if (y < graphHeight - 1) adj = (x, y + 1) :: adj
+
+        adj map {case (x, y) => x + y * graphWidth}
+    }
+
+    private def adjRooms(x: Int, y: Int): List[(Int, Int)] = {
+        val node = x + y * graphWidth
+
+        def isGraphLink(node1: Int, node2: Int) = graphLinks(node1 + node2 * graphSize)
+
+        for (adj_node <- neighbors(node) if isGraphLink(node, adj_node))
+            yield (adj_node % graphWidth, adj_node / graphWidth)
     }
 }
 
--- maze/MazeMain.scala
+++ maze/MazeMain.scala
@@ -7,6 +7,14 @@ object MazeMain {
         val height = args(1).toInt
         val m = new MazeDFS(width, height)
         m.generate
-        println(m.mkString)
+        List.range(0, m.height) foreach {y =>
+            List.range(0, m.width) foreach {x =>
+                if (m.isWall(x, y))
+                    print('#')
+                else
+                    print('.')
+            }
+            println
+        }
     }
 }
