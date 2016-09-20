a8d45383755823b8b80415b2c9f9f0d0dbfbbb20
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2016-09-20 18:26:22 +0200

    rewrite networking to akka (fixed maze size though)

--- server/MazeServer.scala
+++ server/MazeServer.scala
@@ -1,45 +1,56 @@
-import java.net._
-import java.io._
+import akka.actor._
+import akka.io.{IO, Tcp}
+import akka.util.ByteStringBuilder
+import java.net.InetSocketAddress
+import java.nio.ByteOrder
 
 import maze._
 
-object MazeServer {
+class Server extends Actor with ActorLogging {
+    import context.system
 
-    var m: Maze = _
+    IO(Tcp) ! Tcp.Bind(self, new InetSocketAddress("127.0.0.1", 9999))
+
+    def receive = {
+        case Tcp.Bound(localAddress) => log.info("server started")
+
+        case Tcp.CommandFailed(_: Tcp.Bind) => context stop self
+
+        case Tcp.Connected(remote, local) =>
+            log.info("new player connected")
+            val connection = sender()
+            val handler = context.actorOf(Props(classOf[GameHandler], connection))
+    }
+}
+
+class GameHandler(connection: ActorRef) extends Actor with ActorLogging {
+    var m: Maze = new MazeDFS(20, 20)
     var playerX = 0
     var playerY = 0
     var exitX = 0
     var exitY = 0
 
-    def main(args: Array[String]): Unit = {
-        val server = new ServerSocket(9999)
+    implicit val order = ByteOrder.BIG_ENDIAN
 
-        val maxWidth = args(0).toInt
-        val maxHeight = args(1).toInt
+    connection ! Tcp.Register(self)
+    initGame
+    sendMaze
+    sendPlayerPosition
 
-        m = new MazeDFS(maxWidth, maxHeight)
-
-        handleGames(server)
+    def receive = {
+        case Tcp.Received(data) =>
+            processCommand(data(0).toChar)
+            sendPlayerPosition
+            if (playerX == exitX && playerY == exitY) {
+                log.info("player won")
+                context stop self
+            }
+        case Tcp.PeerClosed =>
+            log.info("player disconnected")
+            context stop self
     }
 
-    def handleGames(server: ServerSocket): Unit = {
-        try {
-            println("waiting for new player")
-            val client = server.accept()
-            println("new player has connected")
-
-            initGame()
-            sendMaze(client.getOutputStream())
-            gameLoop(client.getInputStream(), client.getOutputStream())
-            client.close()
-        } catch {
-            case _ : java.net.SocketException => println("connection error")
-        }
-
-        handleGames(server)
-    }
-
-    def initGame() = {
+    private def initGame = {
         m.generate
         playerX = 0
         playerY = 0
@@ -49,48 +60,34 @@ object MazeServer {
         exitY = exits.head
     }
 
-    def sendMaze(out: OutputStream) = {
-        val stream = new DataOutputStream(new BufferedOutputStream(out))
-        stream.writeInt(m.width)
-        stream.writeInt(m.height)
+    private def sendMaze = {
+        var builder = new ByteStringBuilder()
+        builder.putInt(m.width)
+        builder.putInt(m.height)
+
         List.range(0, m.height) foreach {y =>
             List.range(0, m.width) foreach {x =>
                 if (m.isWall(x, y))
-                    stream.write(1)
+                    builder.putByte(1)
                 else
-                    stream.write(0)
+                    builder.putByte(0)
             }
         }
-        stream.writeInt(exitX)
-        stream.writeInt(exitY)
-        stream.flush()
+        builder.putInt(exitX)
+        builder.putInt(exitY)
+
+        connection ! Tcp.Write(builder.result())
     }
 
-    def gameLoop(in: InputStream, out: OutputStream): Unit = {
-        sendPlayerPosition(out)
-
-        if (playerX == exitX && playerY == exitY) {
-            println("player won")
-        } else {
-            val command = in.read()
-            if (command > 0) {
-                processCommand(command.toChar)
-                gameLoop(in, out)
-            } else {
-                println("player disconnected")
-            }
-        }
+    private def sendPlayerPosition = {
+        log.info("sending player pos: " + playerX + " " + playerY)
+        var builder = new ByteStringBuilder()
+        builder.putInt(playerX)
+        builder.putInt(playerY)
+        connection ! Tcp.Write(builder.result())
     }
 
-    def sendPlayerPosition(out: OutputStream) = {
-        println("Sending player pos: " + playerX + " " + playerY)
-        val stream = new DataOutputStream(new BufferedOutputStream(out))
-        stream.writeInt(playerX)
-        stream.writeInt(playerY)
-        stream.flush()
-    }
-
-    def processCommand(command: Char) = command match {
+    private def processCommand(command: Char) = command match {
         case 'w' => {
             if (playerY > 0 && !m.isWall(playerX, playerY - 1))
                 playerY -= 1
@@ -108,5 +105,12 @@ object MazeServer {
                 playerX += 1
         }
         case _ => ()
+    }
+}
+
+object MazeServer {
+    def main(args: Array[String]): Unit = {
+        val system = ActorSystem("MazeServer")
+        val server = system.actorOf(Props[Server], name = "server")
     }
 }
---
+++ server/build.sbt
@@ -0,0 +1,7 @@
+name := "MazeServer"
+ 
+version := "1.0"
+ 
+scalaVersion := "2.11.6"
+ 
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4.10"
